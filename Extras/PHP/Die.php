9b42b0890de1ec8004a10f730863d1632bcfaf42
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-13 22:05:54 -0700

    Die.php
    A PHP script making use of the `die` command.

---
+++ Extras/PHP/Die.php
@@ -0,0 +1,5 @@
+<?php // Start of script
+// Die.php - April 1st 2020 4:38 pm (PHP file, example from w3schools PHP miscellaneous)
+print("Hello world"); // Good (Condition most likely will be met)
+or die("Goodbye world"); // Bad (condition likely won't be met)
+// End of script ?>
